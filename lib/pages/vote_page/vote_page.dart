c476aad376d0f54e75ab291bfe1097235c11c421
Bruno Consani <bruno.ferreira-ext@duratex.com.br>
2022-07-24 15:58:32 -0300

    fix

--- lib/pages/vote_page/vote_page.dart
+++ lib/pages/vote_page/vote_page.dart
@@ -69,28 +69,25 @@ class VotePage extends StatelessWidget {
           const SizedBox(height: 36),
           LayoutBuilder(
             builder: (context, constraints) {
-              return Expanded(
-                flex: getFlexCount(constraints),
-                child: Obx(() {
-                  return GridView.count(
-                    physics: const NeverScrollableScrollPhysics(),
-                    crossAxisCount: getCrossAxisCount(constraints),
-                    shrinkWrap: true,
-                    children: controller.candidates.map((candidate) {
-                      return Center(
-                        child: VoteCard(
-                          description: candidate.name,
-                          imageUrl: candidate.imageUrl,
-                          onVote: controller.canVote.value
-                              ? () => _onTapVote(candidate)
-                              : null,
-                          actionText: 'VOTAR',
-                        ),
-                      );
-                    }).toList(),
-                  );
-                }),
-              );
+              return Obx(() {
+                return GridView.count(
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: getCrossAxisCount(constraints),
+                  shrinkWrap: true,
+                  children: controller.candidates.map((candidate) {
+                    return Center(
+                      child: VoteCard(
+                        description: candidate.name,
+                        imageUrl: candidate.imageUrl,
+                        onVote: controller.canVote.value
+                            ? () => _onTapVote(candidate)
+                            : null,
+                        actionText: 'VOTAR',
+                      ),
+                    );
+                  }).toList(),
+                );
+              });
             },
           ),
         ],
